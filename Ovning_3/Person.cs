8e55c208d26cc013a2a60526720589a2c690281d
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-28 17:02:03 +0200

    Create Person instance. Add argument exception for Age, FName

--- Ovning_3/Person.cs
+++ Ovning_3/Person.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Ovning_3
 {
@@ -13,5 +14,52 @@ namespace Ovning_3
         private string lName;
         private double height;
         private double weight;
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Åldern ska vara större än 0.");
+                age = value;
+            }
+        }
+
+        public string FName
+        {
+            get { return fName; }
+            set 
+            {
+                if (value.Length < 2 || value.Length > 10)
+                    throw new ArgumentException("Förnamn måste vara mellan 2 och 10 tecken.");
+                fName = value;
+            }
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set 
+            {
+
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set { }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { }
+        }
+
     }
+
+    
+
 }
--- Ovning_3/Program.cs
+++ Ovning_3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Person person1 = new Person();
+                person1.Age = 37;
+                person1.FName = "Johan";
+                person1.LName = "Svensson";
+                person1.Height = 1.87;
+                person1.Weight = 75;
+
+                //Kommentar om att det inte går att komma åt variablerna pga de är privata...???
+
+            }
+            
+            catch 
+            { 
+
+            }
         }
     }
 }
