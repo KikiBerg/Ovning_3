da1236703861943b3e2b5c23b075c3de8665766c
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-28 16:07:31 +0200

    Add project files.

---
+++ Ovning_3/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_3
+{
+    public class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+    }
+}
