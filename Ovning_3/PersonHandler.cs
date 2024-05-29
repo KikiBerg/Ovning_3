b3a50b53bdbc52090cb13a32af7e47d47a9370e1
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-29 12:10:51 +0200

    Add PersonHandler class. Change age of person1 via SetAge method from PersonHandlert.

---
+++ Ovning_3/PersonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_3
+{
+    public class PersonHandler
+    {
+        public void SetAge (Person pers, int age) 
+        { 
+            try { pers.Age = age; } 
+            catch (ArgumentException invalidInstance)
+            {
+                string error = invalidInstance.Message;
+                Console.WriteLine($"Ett fel inträffade: {error}");
+            }
+        }
+      
+
+    }
+}
--- Ovning_3/Program.cs
+++ Ovning_3/Program.cs
@@ -6,8 +6,14 @@
         {
             try
             {
+                // Jag instansierar en PersonHandler:
+                PersonHandler personHandler1 = new PersonHandler();
+
                 // Jag instansierar en Person med giltiga värden direkt:
                 Person person1 = new Person(37, "Johan", "Svensson", 1.87, 75);
+
+                // Jag använder PersonHandler för att ändra åldern på person1:
+                personHandler1.SetAge(person1, 45);
             }            
             catch (ArgumentException invalidInstance)
             {
