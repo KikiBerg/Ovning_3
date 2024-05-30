fb9848fc467eb551733c80e76cb0b6aefc388072
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-30 16:29:45 +0200

    Create Animal class

---
+++ Ovning_3/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_3
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+
+        public abstract void DoSound(); // metod
+
+        public Animal(string name, double weight, int age) // konstruktor
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+        }
+    }
+}
